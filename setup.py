54137889ad5cb33cf777778ef16e38f50acb41e5
NateScarlet <NateScarlet@Gmail.com>
2018-03-27 16:15:05 +0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 """Python setup script.  """
+import os
 from setuptools import setup, find_packages
-from cgtwq import __about__
 
+__about__ = {}
+execfile(os.path.join(os.path.dirname(__file__),
+                      'cgtwq', '__about__.py'), __about__)
 setup(
     name='cgtwq',
-    version=__about__.__version__,
-    author=__about__.__author__,
+    version=__about__['__version__'],
+    author=__about__['__author__'],
     packages=find_packages(),
     install_requires=[
         'requests==2.18.4',
         'websocket-client==0.47.0',
-        'git+https://https://github.com/WuLiFang/wlf@bcd210a4843157e33dcf782be74099ce2ca18966#egg=wlf',
-    ]
+        'wlf==0.3.2',
+    ],
+    dependency_links=[
+        ('https://github.com/WuLiFang/wlf/archive/0.3.2.tar.gz'
+         '#egg=wlf-0.3.2'),
+    ],
 )
