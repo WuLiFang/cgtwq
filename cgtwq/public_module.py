9e5439ceb8109c313ba12927f6478fb9ce9d8e99
NateScarlet <NateScarlet@Gmail.com>
2018-05-16 10:32:31 +0800

    Refactor `public_module`

--- cgtwq/public_module.py
+++ cgtwq/public_module.py
@@ -3,72 +3,38 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import logging
-
 from .database import Database
 from .filter import Filter
 from .module import Module
-
-LOGGER = logging.getLogger(__name__)
 
 
 class PublicModule(Module):
     """Module in special `public` database.    """
 
-    def __init__(self):
+    def __init__(self, name, active_filter, name_field):
         self.database = Database('public')
-        super(PublicModule, self).__init__(self.name, self.database)
-
-
-class Project(PublicModule):
-    """Module to keep project information.   """
-
-    name = 'project'
+        self.active_filter = active_filter
+        self.name_field = name_field
+        super(PublicModule, self).__init__(name, self.database)
 
     def all(self):
-        """All active project.
+        """All active entries.
 
         Returns:
-            Selection: Projects.
+            Selection
         """
 
-        return self.filter(Filter('status', 'Active'))
+        return self.filter(self.active_filter)
 
     def names(self):
-        """All actived project names.
+        """All actived entries label.
 
         Returns:
-            tuple
+            tuple[str]
         """
 
-        return self.all()['full_name']
+        return self.all()[self.name_field]
 
 
-PROJECT = Project()
-
-
-class Account(PublicModule):
-    """Module to keep account information.   """
-
-    name = 'account'
-
-    def all(self):
-        """All active user  .
-
-        Returns:
-            Selection: Users.
-        """
-
-        return self.filter(Filter('status', 'Y'))
-
-    def names(self):
-        """All user names.
-
-        Returns:
-            tuple
-        """
-
-        return self.all()['name']
-
-
-ACCOUNT = Account()
+PROJECT = PublicModule('project', Filter('status', 'Active'), 'full_name')
+ACCOUNT = PublicModule('account', Filter('status', 'Y'), 'name')
