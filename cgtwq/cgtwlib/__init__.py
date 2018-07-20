a4e944cfe8737c69e20fae11f4f6afed33e1c347
NateScarlet <NateScarlet@Gmail.com>
2018-07-20 22:00:41 +0800

    Remove unused file

--- cgtwq/cgtwlib/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding=UTF-8 -*-
-"""Library from cgtw offical client.  """
-
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
-from . import http
