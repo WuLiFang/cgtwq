687ff4250705ba095c8ea877d82d6d8f30b8d4e2
NateScarlet <NateScarlet@Gmail.com>
2020-06-23 19:23:40 +0800

    feat: add helper.wlf.get_entry_by_file

--- cgtwq/helper/wlf.py
+++ cgtwq/helper/wlf.py
@@ -4,6 +4,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import logging
+import typing
 
 from six import text_type
 
@@ -14,9 +15,112 @@ from .exceptions import DatabaseError
 
 LOGGER = logging.getLogger(__name__)
 
+if typing.TYPE_CHECKING:
+    pass
 
-class CGTWQHelper(object):
-    """Helper class for cgtwq query.
+
+CACHE = {}
+
+CACHE_KEY_PREFIX_DATABASE_MAP = "prefix_database_map"
+
+
+def guess_entry(select):
+    """Get best matched entry from select.
+
+    Args:
+        select (Selection): CGTeamWork selection.
+
+    Returns:
+        Entry: CGTeamWork entry.
+    """
+
+    current_account_id = cgtwq.current_account_id()
+    data = select.get_fields('id', 'account_id')
+    data = {i[0]: i[1] and i[1].split(',') for i in data}
+    entries = select.to_entries()
+
+    def _by_artist(entry):
+        task_account_id = data[entry[0]]
+        if not task_account_id:
+            return 2
+        if current_account_id in task_account_id:
+            return 0
+        return 1
+
+    entries = sorted(entries, key=_by_artist)
+
+    return entries[0]
+
+
+def get_database_by_file(filename):
+    """Get database name from filename.
+
+    Args:
+        filename (str): Filename.
+
+    Raises:
+        DatabaseError: When can not determinate database from filename.
+
+    Returns:
+        str: Database name.
+    """
+
+    if CACHE_KEY_PREFIX_DATABASE_MAP not in CACHE:
+        data = {}  # type: typing.Dict[text_type, text_type]
+        select = cgtwq.PROJECT.select_activated().get_fields(
+            'code',
+            'database',
+            'last_update_time',
+        )
+        select.sort(key=lambda x: x[2])
+        for i in select:
+            database = cgtwq.Database(i[1])
+            data[i[0].lower() + "_"] = database.name
+            for j in text_type(database.metadata["filename_prefix"] or "").lower().split(","):
+                if not j:
+                    continue
+                data[j] = database.name
+        CACHE[CACHE_KEY_PREFIX_DATABASE_MAP] = data
+    prefix_database_map = CACHE[CACHE_KEY_PREFIX_DATABASE_MAP]
+
+    norm_filename = (text_type(filename)).lower()
+    for k in prefix_database_map:
+        if norm_filename.startswith(k):
+            return prefix_database_map[k]
+
+    raise DatabaseError(
+        'Can not determinate database from filename.', filename)
+
+
+def get_entry_by_file(filename, pipeline, module='shot'):
+    # type: (text_type, text_type, typing.Optional[text_type]) -> cgtwq.Entry
+    """Get entry from filename and pipeline
+
+    Args:
+        filename (str): Filename to determinate shot.
+        pipeline (str): Server defined pipline name.
+        module (str): Defaults to `shot`, Server defined module name.
+
+    Returns:
+        cgtwq.Entry: Entry
+    """
+
+    shot = PurePath(filename).shot
+    database = cgtwq.Database(get_database_by_file(filename))
+    select = database.module(module).filter(
+        (cgtwq.Field('pipeline') == pipeline)
+        & (cgtwq.Field('shot.shot') == shot))
+    try:
+        entry = select.to_entry()
+    except ValueError:
+        LOGGER.warning('Duplicated task: %s', shot)
+        entry = guess_entry(select)
+
+    return entry
+
+
+class CGTWQHelper(object):  # TODO: remove this at next major version.
+    """DEPRECATED: Helper class for cgtwq query.
 
     Attributes:
         prefix_filters: Function list that filter project code to prefix.
@@ -106,7 +210,8 @@ class CGTWQHelper(object):
 
     @staticmethod
     def guess_entry(select):
-        """Get best matched entry from select.
+        """DEPRECATED: use top level `guess_entry` instead.
+        Get best matched entry from select.
 
         Args:
             select (Selection): CGTeamWork selection.
@@ -115,19 +220,4 @@ class CGTWQHelper(object):
             Entry: CGTeamWork entry.
         """
 
-        current_account_id = cgtwq.current_account_id()
-        data = select.get_fields('id', 'account_id')
-        data = {i[0]: i[1] and i[1].split(',') for i in data}
-        entries = select.to_entries()
-
-        def _by_artist(entry):
-            task_account_id = data[entry[0]]
-            if not task_account_id:
-                return 2
-            if current_account_id in task_account_id:
-                return 0
-            return 1
-
-        entries = sorted(entries, key=_by_artist)
-
-        return entries[0]
+        return guess_entry(select)
--- tests/test_helper_wlf.py
+++ tests/test_helper_wlf.py
@@ -7,16 +7,17 @@ from __future__ import (absolute_import, division, print_function,
 import pytest
 
 import cgtwq.helper.wlf
-
 from tests import util
 
 
 @util.skip_if_not_logged_in
 def test_get_database():
-    assert cgtwq.helper.wlf.CGTWQHelper.get_database(
+    assert cgtwq.helper.wlf.get_database_by_file(
         'MT_example.jpg') == 'proj_mt'
-    assert cgtwq.helper.wlf.CGTWQHelper.get_database(
+    assert cgtwq.helper.wlf.get_database_by_file(
         'MT2_example.jpg') == 'proj_mt2'
+    assert cgtwq.helper.wlf.get_database_by_file(
+        'JSL_example.jpg') == 'proj_slj'
 
     with pytest.raises(cgtwq.helper.wlf.DatabaseError):
         cgtwq.helper.wlf.CGTWQHelper.get_database('example.jpg')
