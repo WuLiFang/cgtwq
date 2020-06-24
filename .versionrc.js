85d394b7f5793e0d552b126ccaff5343adb01e40
NateScarlet <NateScarlet@Gmail.com>
2020-06-24 12:03:23 +0800

    docs: update versions

---
+++ .versionrc.js
@@ -0,0 +1,20 @@
+// https://github.com/conventional-changelog/conventional-changelog-config-spec/blob/master/versions/2.1.0/README.md#compareurlformat-string
+
+const URL = "https://github.com/WuLiFang/cgtwq";
+
+module.exports = {
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "chore", hidden: true },
+    { type: "docs", hidden: true },
+    { type: "style", hidden: true },
+    { type: "refactor", hidden: true },
+    { type: "perf", section: "Performance" },
+    { type: "test", hidden: true },
+  ],
+  commitUrlFormat: `${URL}/commit/{{hash}}`,
+  compareUrlFormat: `${URL}/compare/{{previousTag}}...{{currentTag}}`,
+  issueUrlFormat: `${URL}/issues/{{id}}`,
+  userUrlFormat: `https://github.com//{{user}}`,
+};
--- cgtwq/__about__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding=UTF-8 -*-
-"""CGTeamWork python client for humans.  """
-
-__version__ = '3.0.0-beta.0'
-__author__ = 'NateScarlet@Gmail.com'
